fff2d3adf01e2189b1dde1ebb187b889662450bd
Alena Shkurenko <alenashkurenko@kostyas-MacBook-Pro.local>
2023-09-29 18:26:33 +0200

    add solutiiion modal

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -1,19 +1,22 @@
-(function() {
-    const close = document.getElementById('modal-close');
-    const modal = document.getElementById('modal');
-    const modalOpen = document.getElementById('modal-open');
+(function () {
+  const close = document.getElementById('modal-close');
+  const modal = document.getElementById('modal');
+  const modalOpen = document.getElementById('modal-open');
+  const modalBackground = document.getElementById('modal-background');
 
-    if (!close || !modalOpen || !modal) {
-        return;
-    }
+  if (!close || !modalOpen || !modal) {
+    return;
+  }
 
-    modalOpen.addEventListener('click', () => {
-        modal.style.display = 'block';
-        document.body.overflow = 'hidden';
-    });
+  modalOpen.addEventListener('click', () => {
+    modal.style.display = 'block';
+    modalBackground.style.display = 'block';
+    document.body.overflow = 'hidden';
+  });
 
-    close.addEventListener('click', () => {
-        modal.style.display = 'none';
-        document.body.overflow = 'initial';
-    });
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modalBackground.style.display = 'none';
+    document.body.overflow = 'initial';
+  });
 })();
