4dec827ac2b5f07d4f105f373cab346226ede833
Alena Shkurenko <alenashkurenko@kostyas-MacBook-Pro.local>
2023-10-03 09:07:45 +0200

    fix pr comments

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,7 +2,6 @@
   const close = document.getElementById('modal-close');
   const modal = document.getElementById('modal');
   const modalOpen = document.getElementById('modal-open');
-  const modalBackground = document.getElementById('modal-background');
 
   if (!close || !modalOpen || !modal) {
     return;
@@ -10,13 +9,11 @@
 
   modalOpen.addEventListener('click', () => {
     modal.style.display = 'block';
-    modalBackground.style.display = 'block';
     document.body.overflow = 'hidden';
   });
 
   close.addEventListener('click', () => {
     modal.style.display = 'none';
-    modalBackground.style.display = 'none';
     document.body.overflow = 'initial';
   });
 })();
